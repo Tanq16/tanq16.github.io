1858675911f8d63ceddd8487e34a099d093c5343
Tanq16 <37408906+Tanq16@users.noreply.github.com>
2026-01-05 08:49:06 -0500

    nit

--- resume/js/definitions.js
+++ resume/js/definitions.js
@@ -1,7 +1,7 @@
 const RESUME_DATA = {
     basics: {
         name: "TANISHQ RUPAAL",
-        email: "<a href='mailto:dragonking47@proton.me' target='_blank'>dragonking47@proton.me</a>",
+        email: "<a href='mailto:trupaal+resume@gmail.com' target='_blank'>trupaal@gmail.com</a>",
         website: "<a href='https://tanishq.page' target='_blank'>tanishq.page</a>",
         // location: "Earth, Solar System, Milky Way Galaxy",
         // phone: "+1 (555) 555-5555",
@@ -32,7 +32,7 @@ const RESUME_DATA = {
                     subtitle: "Senior Security Engineer",
                     date: "Oct 2022 - Oct 2024",
                     details: [
-                        "Published a AWS security-focussed blog post on ABAC in Lambda",
+                        "Published a AWS security-focussed blog post on <a href='https://www.praetorian.com/blog/abac-in-lambda/' target='_blank'>ABAC in Lambda</a>",
                         "Lead client interviews to understand and document details of client systems",
                         "Improved quality of assessment reports for the cloud security service line"
                     ]
@@ -60,8 +60,8 @@ const RESUME_DATA = {
                     subtitle: "Security Engineer Intern",
                     date: "May 2020 - Aug 2020",
                     details: [
-                        "Improved Fuzzbench to use single source of truth for benchmark container builds",
-                        "Eliminate differences between standard and OSS-Fuzz benchmark integration"
+                        "Improved <a href='https://github.com/google/fuzzbench' target='_blank'>Fuzzbench</a> to use single source of truth for benchmark container builds",
+                        "Eliminate differences between standard and <a href='https://github.com/google/oss-fuzz' target='_blank'>OSS-Fuzz</a> benchmark integration"
                     ]
                 },
                 {
@@ -117,22 +117,22 @@ const RESUME_DATA = {
                 {
                     title: "Security Tooling",
                     details: [
-                        "<b>Nebula</b> - Multi-cloud security testing tool for AWS, GCP, Azure, and K8s with IAM graph analysis",
-                        "<b>Containerized Security Toolkit</b> - Docker container with collection of security tools and workflow for security operations"
+                        "<b><a href='https://github.com/praetorian-inc/nebula' target='_blank'>Nebula</a></b> (contributor) - Multi-cloud security testing tool for AWS, GCP, Azure, and K8s with IAM graph analysis",
+                        "<b><a href='https://github.com/tanq16/containerized-security-toolkit' target='_blank'>Containerized Security Toolkit</a></b> - Docker container with collection of security tools and workflow for security operations"
                     ]
                 },
                 {
                     title: "Personal Projects",
                     details: [
-                        "<b>Expense Owl</b> - Self-hosted expense tracking web app built with Go",
-                        "<b>Local-Content-Share</b> - Self-hosted app for sharing text & files in LAN with integrated notepad",
-                        "<b>AI Context</b> - CLI tool to generate AI-friendly context from code repos, webpages, or YouTube videos",
+                        "<b><a href='https://github.com/Tanq16/ExpenseOwl' target='_blank'>Expense Owl</a></b> - Self-hosted expense tracking web app built with Go",
+                        "<b><a href='https://github.com/Tanq16/local-content-share' target='_blank'>Local-Content-Share</a></b> - Self-hosted app for sharing text & files in LAN with integrated notepad",
+                        "<b><a href='https://github.com/Tanq16/ai-context' target='_blank'>AI Context</a></b> - CLI tool to generate AI-friendly context from code repos and directories, webpages, or YouTube videos",
                     ]
                 },
                 {
                     title: "Exploratory Projects",
                     details: [
-                        "<b>SubDextract</b> - Sub-domain enumeration tool in Python with CT logs, SAN, DNS queries",
+                        "<b><a href='https://github.com/Tanq16/subdextract' target='_blank'>SubDextract</a></b> - Sub-domain enumeration tool in Python with CT logs, SAN, DNS queries",
                         "<b>Analysis of Top 1 Million Domains</b> from Majestic, Alexa, Tranco lists for HTTP/2.0, IPv6, TLS adoption"
                     ]
                 }
